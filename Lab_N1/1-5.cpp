3e4b71a0519bde034ab05d5a487b4a0f2a70685c
Mikhail Shestakov <mishail258@mail.ru>
2025-09-24 12:38:25 +0500

    update

--- Lab_N1/1-5.cpp
+++ Lab_N1/1-5.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 bool is2Digits(int x) {
 	int div = (int)x / 10;
-	cout << div << endl;
 	if (div <= 9 && div != 0) {
 		return true;
 	}
@@ -17,7 +16,6 @@ int main() {
 	cout << "Enter your number: " << endl;
 	cin >> number;
 	
-	cout << number << endl;
 	
 	if (is2Digits(number)) {
 		cout << "true";
