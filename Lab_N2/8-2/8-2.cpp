c9d207854ce5df93f5bb7a38e067983c864aed5e
Mikhail Shestakov <mishail258@mail.ru>
2025-10-08 14:31:37 +0500

    da

--- Lab_N2/8-2/8-2.cpp
+++ Lab_N2/8-2/8-2.cpp
@@ -17,21 +17,7 @@ int main(){
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
         }
     }
-    for (int i {}; i < lstLenght; i++){
-        std::string value {};
-        std::cout << "Enter " << i+1 << " value: ";
-        while (true){
-            if (std::cin >> value){
-                break;
-            }
-            else{
-                std::cout << "Invalid input. Input valid value"<< std::endl;
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            }
-        }
-        lst.push_back(value);
-    }
+
     std::deque<std::string> deq {solution((lst))};
     for (auto& i : deq){
         std::cout << i << " ";
