596380aaf828e16ec84e1cc510cc48fa25f76b6f
Mikhail Shestakov <mishail258@mail.ru>
2025-10-08 14:31:52 +0500

    da

--- Lab_N2/5-5/5-5.cpp
+++ Lab_N2/5-5/5-5.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <list>
 #include <algorithm>
-
+#include <limits>
 
 int main(){
-    std::list<int> lst {-1,-2,-3,4,5,10};
+    std::list<int> lst {};
+    int LstLenght {};
+    while (true){
+        if (std::cin >> LstLenght && LstLenght > 0){
+            break;
+        }
+        else{
+            std::cout << "Invalid input. Input valid value" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+        }
+    }
+    for (int i {}; i < LstLenght; i++){
+        int value {};
+        std::cout << "Input " << i+1 << " number: ";
+        while (true){
+            if (std::cin>>value){
+                break;
+            }
+            else{
+                std::cout << "Invalid input. Input valid value." << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+
+            }
+        }
+        lst.push_back(value);
+    }
     auto negative = std::find_if(lst.begin(), lst.end(), [](int x){ return x < 0;});
     lst.insert(negative, 0);
     auto positive = std::find_if(lst.rbegin(), lst.rend(), [](int x){ return x > 0;});
